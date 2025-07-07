00684cf19fddcfe3c3912c59ddafe4eb188c4a67
pinocure <xodiq@naver.com>
2025-07-07 22:23:32 +0900

    final
    final

--- src/main/java/com/ruang/hh99/service/PostService.java
+++ src/main/java/com/ruang/hh99/service/PostService.java
@@ -72,11 +72,6 @@ public class PostService {
 
     // 선택한 게시글 조회
     public PostResponse getPost(Integer postId) {
-//        Object[] result = postRepository.findPostWithUserInfoById(postId)
-//                .orElseThrow(() -> new RuntimeException("게시글을 찾을 수 없습니다."));
-//
-//        Post post = (Post) result[0];
-//        String userName = (String) result[1];
 
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new RuntimeException("게시글을 찾을 수 없습니다."));
